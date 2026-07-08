056cdca0d5246c2b4ff7c6eae92a5d3625392286
red456loto-star <red456loto@gmail.com>
2026-07-08 03:17:46 -0500

    Fase 10: cargar estilos de reportes y correo

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -8,6 +8,7 @@ import "./phase2-forms.css";
 import "./phase2-tree.css";
 import "./phase4-import.css";
 import "./phase9-dashboard.css";
+import "./phase10.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
